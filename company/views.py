27abfab3fd0b2a86ac665ecf8a96b9e16caae1b8
Andre Marques <andre.luism00@gmail.com>
2021-03-07 20:26:37 -0300

    count for company

--- company/views.py
+++ company/views.py
@@ -1,11 +1,11 @@
-from rest_framework import serializers, viewsets, response
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import serializers, viewsets
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 from rest_framework.views import APIView
-
 from client.models import Client
 from company.models import Company
 from lawsuit.models import Locker, Lawsuit
-from user.models import User
 
 
 class CompanySerializer(serializers.ModelSerializer):
@@ -20,16 +20,30 @@ class CompanyView(viewsets.ModelViewSet):
     permission_classes = [AllowAny]
 
 
-class CompanyTotalView(APIView):
+class CompanyTotalSerializer(serializers.Serializer):
+    id = serializers.IntegerField(required=True)
 
+
+class CompanyTotalOutputSerializer(serializers.Serializer):
+    count_client = serializers.IntegerField()
+    count_locker = serializers.IntegerField()
+    count_lawsuit = serializers.IntegerField()
+
+
+class CompanyTotalView(APIView):
+    serializer_class = CompanyTotalSerializer
+
+    @swagger_auto_schema(request_body=CompanyTotalSerializer,
+                         responses={200: CompanyTotalOutputSerializer})
     def post(self, request):
-        company_id = 1
+        company_id = request.data.get('company_id')
         count_client = Client.objects.filter(company=company_id).count()
         count_locker = Locker.objects.filter(company=company_id).count()
         count_lawsuit = Lawsuit.objects.filter(company=company_id).count()
-        response.data = {
+        rsp = Response()
+        rsp.data = {
             'count_client': count_client,
             'count_locker': count_locker,
             'count_lawsuit': count_lawsuit
         }
-        return response
+        return rsp
