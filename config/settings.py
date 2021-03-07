e7eeca440fd361e4116d141090c8f5663aa9b174
Andre Marques <andre.luism00@gmail.com>
2021-03-07 18:32:15 -0300

    allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
