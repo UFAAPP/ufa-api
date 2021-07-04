550172e1e2d10f5bd8c29a987586e97132988b36
Bruno <bruno.vasconcelos@nexxera.com>
2021-07-04 02:18:31 -0300

    refactor: added try-except when create user

--- user/services.py
+++ user/services.py
@@ -1,10 +1,16 @@
+from django.db import IntegrityError
+from rest_framework.exceptions import ValidationError
+
 from user.models import User
 
 
 class UserService:
     @staticmethod
     def create_user(*, first_name: str, last_name: str, email: str, social_number: str, password: str) -> User:
-        user = User.objects.create_user(first_name=first_name, last_name=last_name, email=email,
-                                        is_active=True, social_number=social_number,
-                                        username=email, company=None, password=password)
+        try:
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, email=email,
+                                            is_active=True, social_number=social_number,
+                                            username=email, company=None, password=password)
+        except IntegrityError:
+            raise ValidationError(f'User with email: {email} already exists.')
         return user
